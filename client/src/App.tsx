ab8a3de16576f8d77ba626a12b14e627ba5758a7
tuNovio <alfgomachi@gmail.com>
2023-07-11 12:38:34 -0500

    componente principal de la pagina

---
+++ client/src/App.tsx
@@ -0,0 +1,50 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { httpBatchLink } from '@trpc/client'
+import { useState } from 'react'
+import { trpc } from './trpc'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+//css archivos
+import './App.css'
+
+//componentes
+import UserForm from './components/userForm'
+import ListaUsuarios from './components/listaUsuario'
+import Login from './components/Login'
+
+//routes from components
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <ListaUsuarios />
+	},
+	{
+		path: "/login",
+		element: <Login />
+	},
+	{
+		path: "/registro",
+		element: <UserForm />
+	}
+])
+
+function App() {
+	const [queryClient] = useState(() => new QueryClient());
+	const [trpcClient] = useState(() =>
+		trpc.createClient({
+			links: [
+				httpBatchLink({
+					url: 'http://localhost:3000/trpc',
+				}),
+			],
+		}),
+	);
+	return (
+		<trpc.Provider client={trpcClient} queryClient={queryClient}>
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+			</QueryClientProvider>
+		</trpc.Provider>
+	);
+}
+
+export default App
