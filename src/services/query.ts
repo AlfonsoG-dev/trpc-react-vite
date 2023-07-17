ea5fe5764f5ca8571b05c23f58e05130fa700e59
tuNovio <alfgomachi@gmail.com>
2023-07-17 18:33:48 -0500

    la dependencia OkPacket esta en desuso por lo tanto se pasa a un RowDataPacket extendido para los datos de la cuneta

--- src/services/query.ts
+++ src/services/query.ts
@@ -1,40 +1,49 @@
+import mysql from 'mysql2';
 import { QueryError, RowDataPacket, OkPacket } from 'mysql2';
 import * as model from '../models/myModel';
 import { connect } from './db';
+interface Cuenta extends mysql.RowDataPacket {
+	id: number;
+	nombre: string;
+	email: string;
+	password: string;
+	rol: string;
+
+}
 
 const conn = connect();
 
 export class UserRepository {
 	// lista de todos los usuarios de la db
-	readAll(): Promise<model.User[]> {
+	readAll(): Promise<model.User[] | any | undefined> {
 		return new Promise((resolve, reject) => {
-			conn.execute('select * from user', (err: QueryError, res) => {
+			conn.execute('select * from user', (err: QueryError, res: any) => {
 				if (err) reject(err);
 				else resolve(res)
 			})
 		})
 	}
 	// usuario dado por id
-	readById(user_id: number): Promise<model.User> {
+	readById(user_id: number): Promise<model.User | undefined> {
 		return new Promise((resolve, reject) => {
-			conn.execute('select * from user where id = ?', [user_id], (err, res) => {
+			conn.execute('select * from user where id = ?', [user_id], (err, res: Cuenta[]) => {
 				if (err) reject(err);
-				else resolve(res?.[0]);
+				else resolve(res?.[0]['']);
 			})
 		})
 	}
-	readUserByName(user_name: string): Promise<model.User> {
+	readUserByName(user_name: string): Promise<model.User | undefined> {
 		return new Promise((resolve, reject) => {
-			conn.execute('select * from user where nombre = ?', [user_name], (err, res) => {
+			conn.execute('select * from user where nombre = ?', [user_name], (err, res: Cuenta[]) => {
 				if (err) reject(err);
-				else resolve(res?.[0]);
+				else resolve(res?.[0]['']);
 			})
 		})
 	}
 	// crear un usuario nuevo
 	create(user: model.User): Promise<model.User> {
 		return new Promise((resolve, reject) => {
-			conn.query<OkPacket>('insert into user (nombre, email, password, rol, create_at) values (?, ?, ?, ?, ?)', [user.nombre, user.email, user.password, user.rol, user.create_at], (err, res) => {
+			conn.query<number | any>('insert into user (nombre, email, password, rol, create_at) values (?, ?, ?, ?, ?)', [user.nombre, user.email, user.password, user.rol, user.create_at], (err, res) => {
 				if (err) reject(err);
 				else
 					this.readById(res.insertId)
@@ -46,7 +55,7 @@ export class UserRepository {
 	// modificar los datos del usuario
 	update(user: model.User): Promise<model.User | undefined> {
 		return new Promise((resolve, reject) => {
-			conn.query<OkPacket>('update user set nombre =?, email =?, password =?, rol =?, create_at =? where id = ?', [user.nombre, user.email, user.password, user.rol, user.create_at, user.id], (err, res) => {
+			conn.query<Cuenta[]>('update user set nombre =?, email =?, password =?, rol =?, create_at =? where id = ?', [user.nombre, user.email, user.password, user.rol, user.create_at, user.id], (err, res) => {
 				if (err) reject(err);
 				else
 					this.readById(user.id!)
@@ -56,9 +65,9 @@ export class UserRepository {
 		})
 	}
 	// eliminar el usuario por el identificador
-	remove(user_id: number): Promise<number> {
+	remove(user_id: number): Promise<number | undefined> {
 		return new Promise((resolve, reject) => {
-			conn.query<OkPacket>('delete from user where id = ?', [user_id], (err, res) => {
+			conn.query<any>('delete from user where id = ?', [user_id], (err, res) => {
 				if (err) reject(err);
 				else resolve(res.affectedRows);
 			})
